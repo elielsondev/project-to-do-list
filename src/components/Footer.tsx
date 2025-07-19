17dc94d5fe8df9e7116283a102f4a4b4fb9c0e87
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-07-19 13:22:27 -0300

    feat and style - implemetation and style in component Footer'

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="text-center">
+            <h5 className="arsenal-regular-italic">Projeto desenvolvido por Elielson Nascimento - Utilizando React e Tailwind</h5>
+        </footer>
+    );
+}
